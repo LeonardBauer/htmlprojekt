c5238e67a57795c43bb3976bf08c653f5db3787c
everythxxgs <leochriba@gmail.com>
2024-03-02 00:41:34 +0100

    Add form reset functionality and style enhancements to contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -10,6 +10,7 @@
 		const formData = new FormData(data.currentTarget)
 		const object = Object.fromEntries(formData);
 		const json = JSON.stringify(object);
+
 		
 		const response = await fetch("https://api.web3forms.com/submit", {
 			method: "POST",
@@ -23,16 +24,37 @@
 		if (result.success) {
 			console.log(result);
 			status = result.message || "Success"
+			document.getElementById("form").childNodes.forEach((el) => {
+				if (el.tagName === "INPUT" || el.tagName === "TEXTAREA") {
+					el.value = "";
+				}
+			});
 		}
 	}
 
 </script>
-<form on:submit|preventDefault={handleSubmit}>
+<form on:submit|preventDefault={handleSubmit} id="form">
 	<input type="hidden" id="accessKey" name="access_key" value={PUBLIC_ACCESSKEY}>
 	<input type="text" class="bg-accent/10" name="name" placeholder="John Doe" required />
 	<input type="email" class="bg-accent/10" name="email" placeholder="john.doe@gmail.com" required />
-	<textarea name="message" class="bg-accent/10" required rows="3"></textarea>
+	<textarea name="message" class="bg-accent/10" required rows="3" placeholder="Message..."></textarea>
 	<input type="submit" />
 </form>
 
 <div>{status}</div>
+
+
+<style lang="postcss">
+	form{
+		display: flex;
+		flex-direction: column;
+		& > *{
+			margin-bottom: 1rem;
+			padding: 0.5rem;
+			border: none;
+			border-radius: 0.5rem;
+		}
+	}
+	
+
+</style>
